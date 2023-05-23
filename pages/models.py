f37efa6830afcb08be66c84b0ceb47867b142ca4
Yizhao Luan <luanyz_leo@163.com>
2023-05-23 16:39:06 +0800

    中间版本 browse-results添加subcluster marker 表格

--- pages/models.py
+++ pages/models.py
@@ -30,4 +30,76 @@ class Study(models.Model):
     def __str__(self):
         return self.index
 
+# # model for Sample table.
+# class Sample(models.Model):
 
+# model for SingleCell.
+class SingleCell(models.Model):
+    dataset = models.CharField(max_length=150)
+    donor = models.CharField(max_length=150)
+    sample = models.CharField(max_length=150)
+    cell_barcode = models.CharField(max_length=150)
+    ncount_rna = models.IntegerField()
+    nfeature_rna = models.IntegerField()
+    seurat_cluster = models.IntegerField()
+    singler_celltype = models.CharField(max_length=150)
+    final_major_celltype = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.dataset
+
+
+# model for Diff_marker_subcluster table.
+class Marker_Subcluster(models.Model):
+    dataset = models.CharField(max_length=150)
+    cluster = models.CharField(max_length=150)
+    gene = models.CharField(max_length=150)
+    avg_log2FC = models.FloatField()
+    pct1 = models.FloatField()
+    pct2 = models.FloatField()
+    pval = models.FloatField()
+    padj = models.FloatField()
+
+    def __str__(self):
+        return self.dataset
+
+# model for Diff_marker_celltype table.
+class Marker_Celltype(models.Model):
+    dataset = models.CharField(max_length=150)
+    cluster = models.CharField(max_length=150)
+    gene = models.CharField(max_length=150)
+    avg_log2FC = models.DecimalField(max_digits=100, decimal_places=4)
+    pct1 = models.DecimalField(max_digits=100, decimal_places=4)
+    pct2 = models.DecimalField(max_digits=100, decimal_places=4)
+    pval = models.DecimalField(max_digits=100, decimal_places=4)
+    padj = models.DecimalField(max_digits=100, decimal_places=4)
+
+    def __str__(self):
+        return self.dataset
+
+# model for LRpairs.
+class LRpairs(models.Model):
+    dataset = models.CharField(max_length=200)
+    source = models.CharField(max_length=200)
+    target = models.CharField(max_length=200)
+    ligand = models.CharField(max_length=200)
+    receptor = models.CharField(max_length=200)
+    prob = models.DecimalField(max_digits=100, decimal_places=4)
+    pval = models.DecimalField(max_digits=100, decimal_places=4)
+    pathway = models.CharField(max_length=200)
+    evidence = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.dataset
+
+# model for SignalPathway.
+class SignalPathway(models.Model):
+    dataset = models.CharField(max_length=200)
+    source = models.CharField(max_length=200)
+    target = models.CharField(max_length=200)
+    pathway = models.CharField(max_length=200)
+    prob = models.DecimalField(max_digits=100, decimal_places=4)
+    pval = models.DecimalField(max_digits=100, decimal_places=4)
+
+    def __str__(self):
+        return self.dataset
--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Employee
 from .models import Study
+from .models import Marker_Subcluster
+from .models import Marker_Celltype
+from .models import LRpairs
+from .models import SingleCell
+from .models import SignalPathway
+
 
 # Create your views here.
 def index(request):
@@ -18,19 +24,28 @@ def browse(request):
     return render(request, 'browse.html', {"studies": Study.objects.all()}) # need to define Study class in models.py.
 
 def browse_results(request):
-    value = request.GET.get('value', '')
+    value_dt_selected = request.GET.get('value', '')
+
+    # subset rows from Marker_Subcluster table.
+    try:
+        row_subcluster_marker = Marker_Subcluster.objects.filter(dataset=value_dt_selected)
+    except Marker_Subcluster.DoesNotExist:
+        row_subcluster_marker = None
     dataset_title = 'Deficiency of ribosomal proteins reshapes the transcriptional and translational landscape in human cells'
     dataset_organism = "Human"
     dataset_geo = "hahha"
     dataset_design = "To be downloaded"
 
     return render(request, "browse_results.html", {
-        'value': value,
+        'value': value_dt_selected,
         'dataset_title': dataset_title,
         'dataset_organism': dataset_organism,
         'dataset_geo': dataset_geo,
         'dataset_design': dataset_design,
-        'studies': Study.objects.all()
+        'row_subcluster_marker': row_subcluster_marker,
+        'major_celltype_marker': Marker_Celltype.objects.all(),
+        'LRpairs': LRpairs.objects.all(),
+        'signal_pathway': SignalPathway.objects.all()
     } )
 
 # views for goto search page from the sidebar.
